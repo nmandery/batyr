c11e9b592d66954274c58a867cb4466c75b29eb3
Nico Mandery <nico.mandery@geops.de>
2015-02-18 09:54:54 +0100

    build system: check and search for markdown converter during configuration

--- src/web/generate-manual-html.py
+++ src/web/generate-manual-html.py
@@ -4,7 +4,10 @@
 
 import sys
 import subprocess
+import os
+
+markdown_binary = os.environ.get("MARKDOWN_EXECUTABLE", "markdown")
 
 template_html = open(sys.argv[2]).read()
-rendered_manual = subprocess.check_output(['markdown', '-f', '+autolink', sys.argv[1]])
+rendered_manual = subprocess.check_output([markdown_binary, '-f', '+autolink', sys.argv[1]])
 open(sys.argv[3], 'w').write(template_html.replace('@MANUAL_HTML@', rendered_manual))
